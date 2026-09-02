130e1ce839c477d0d4460a7e2a29c05cc91621b2
Daniel Martínez Cisneros <daniel.martinez@liferay.com>
2026-09-02 10:39:15 +0200

    Fix table status colors

--- userscript.js
+++ userscript.js
@@ -3,7 +3,7 @@
 // @author       Ally, Rita, Dmcisneros
 // @icon         https://www.liferay.com/o/classic-theme/images/favicon.ico
 // @namespace    https://liferay.atlassian.net/
-// @version      3.27
+// @version      3.28
 // @description  Jira statuses + Patcher, Account tickets and CP Link field + Internal Note highlight + Auto Expand CCC Info + colorize solution proposed + Internal Request Warning + Large File Attachment section
 // @match        https://liferay.atlassian.net/*
 // @match        https://liferay-sandbox-424.atlassian.net/*
@@ -55,6 +55,7 @@
             'div[aria-label*="Status"],' +
             '[data-testid*="issue-status"] span,' +
             '[data-testid$="status-button.status-button"],' + // NEW: the new status button element itself
+            '[data-testid*="cell-wrapper-row"][data-testid$="-status"] button,' + // NEW: queue table status button, at-rest state (no data-testid/title/aria-label until hovered/focused)
             '.css-1mh9skp,' +
             '.css-14er0c4,' +
             '.css-1ei6h1c'
@@ -1093,8 +1094,8 @@
         };
     };
 
-    const throttledUpdateUI = createThrottler(() => updateUI(), 150); 
+    const throttledUpdateUI = createThrottler(() => updateUI(), 150);
     const observer = new MutationObserver(throttledUpdateUI);
-    observer.observe(document.body, { childList: true, subtree: true });
+    observer.observe(document.body, { childList: true, subtree: true, characterData: true });
 
 })();
